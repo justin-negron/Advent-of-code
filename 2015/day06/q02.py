3162309ea33d981aa751a8be4d7fdc5f8a829cca
dexterity74 <justin.704504@gmail.com>
2025-04-02 23:51:39 -0500

    2015 day 6 init commit

---
+++ 2015/day06/q02.py
@@ -0,0 +1,13 @@
+"""
+--- Day 6: Probably a Fire Hazard ---
+https://adventofcode.com/2015/day/6
+"""
+from aocd.models import Puzzle
+import sys
+import hashlib
+
+
+p = Puzzle(day=6, year=2015)
+input = p.input_data
+
+
