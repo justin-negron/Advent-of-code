3bfb103021fd16bc333f5bd10615f2842d069a10
dexterity74 <justin.704504@gmail.com>
2025-04-05 23:44:51 -0500

    init 2015 day7 & day8

---
+++ 2015/day07/q02.py
@@ -0,0 +1,12 @@
+"""
+--- Day 7: Some Assembly Required ---
+https://adventofcode.com/2015/day/7
+"""
+from aocd.models import Puzzle
+
+
+p = Puzzle(day=7, year=2015)
+input = p.input_data.splitlines()
+
+
+# p.answer_b = 
---
+++ 2015/day08/q02.py
@@ -0,0 +1,12 @@
+"""
+--- Day 8: Matchsticks ---
+https://adventofcode.com/2015/day/8
+"""
+from aocd.models import Puzzle
+
+
+p = Puzzle(day=8, year=2015)
+input = p.input_data.splitlines()
+
+
+# p.answer_b = 
