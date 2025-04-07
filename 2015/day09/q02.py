861d4448a1350ee133ec75816b95f6c7c7198af6
dexterity74 <justin.704504@gmail.com>
2025-04-06 22:40:56 -0500

    init 2015 day09

---
+++ 2015/day09/q02.py
@@ -0,0 +1,12 @@
+"""
+--- Day 9: All in a Single Night ---
+https://adventofcode.com/2015/day/9
+"""
+from aocd.models import Puzzle
+
+
+p = Puzzle(day=9, year=2015)
+input = p.input_data.splitlines()
+
+
+# p.answer_b = 
