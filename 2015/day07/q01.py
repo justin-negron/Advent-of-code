0bf272f34b56cb446874dc5ba41dd0ee111186d3
dexterity74 <justin.704504@gmail.com>
2025-04-07 22:32:19 -0500

    cleanup 2015 day07 part 1

--- 2015/day07/q01.py
+++ 2015/day07/q01.py
@@ -12,17 +12,20 @@ input = p.input_data.splitlines()
 q = deque(input)
 var_map = dict()
 
+def get(var):
+    return var_map[var] if var in var_map else np.uint16(var)
+
 while q:
     line = q.pop()
     try:
         match line.split():
             case n, "->", w:
-                var_map[w] = var_map[n] if n in var_map else np.uint16(n)
+                var_map[w] = get(n)
             case "NOT", n, "->", w:
-                var_map[w] = ~var_map[n] if n in var_map else ~np.uint16(n)
+                var_map[w] = ~get(n)
             case x, op, y, "->", w:
                 f = getattr(np.uint16, f"__{op}__".lower())
-                var_map[w] = f(var_map[x] if x in var_map else np.uint16(x), var_map[y] if y in var_map else np.uint16(y))
+                var_map[w] = f(get(x), get(y))
     except (KeyError, ValueError):
         q.appendleft(line)
 
