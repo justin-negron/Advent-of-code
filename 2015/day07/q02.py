3d22de74267b76e0f38ebff68debd1d4da5dc7f7
dexterity74 <justin.704504@gmail.com>
2025-04-07 22:49:38 -0500

    finished 2015 day07 part 2

--- 2015/day07/q02.py
+++ 2015/day07/q02.py
@@ -3,10 +3,41 @@
 https://adventofcode.com/2015/day/7
 """
 from aocd.models import Puzzle
+from collections import deque
+import numpy as np
 
 
 p = Puzzle(day=7, year=2015)
 input = p.input_data.splitlines()
 
+var_map = dict()
 
-# p.answer_b = 
+def assembly(q, wire):
+    
+    def get(var):
+        return var_map[var] if var in var_map else np.uint16(var)
+
+    while q:
+        line = q.pop()
+        try:
+            match line.split():
+                case n, "->", w:
+                    var_map[w] = get(n)
+                case "NOT", n, "->", w:
+                    var_map[w] = ~get(n)
+                case x, op, y, "->", w:
+                    f = getattr(np.uint16, f"__{op}__".lower())
+                    var_map[w] = f(get(x), get(y))
+        except (KeyError, ValueError):
+            q.appendleft(line)
+
+    return var_map[wire]
+
+# take signal of wire a
+wire_a = assembly(deque(input), "a")
+# reset other wires
+var_map = dict()
+# override wire b with previous wire a
+var_map["b"] = wire_a
+# what is new signal of wire a?
+p.answer_b = assembly(deque(input), "a")
