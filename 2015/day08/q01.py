ade6f05264f30be7c6bd08deb0542fe3059f02de
dexterity74 <justin.704504@gmail.com>
2025-04-07 23:49:00 -0500

    finished 2015 day08 part1

--- 2015/day08/q01.py
+++ 2015/day08/q01.py
@@ -8,5 +8,21 @@ from aocd.models import Puzzle
 p = Puzzle(day=8, year=2015)
 input = p.input_data.splitlines()
 
+total_literals = 0
+total_memory = 0
 
-# p.answer_a = 
+for line in input:
+    total_literals += len(line)
+    i = 1
+    while i < len(line)-1:
+        if line[i] == "\\" and line[i+1] == "x":
+            total_memory += 1
+            i += 4
+        elif line[i] == "\\" and i+1 != len(line):
+            total_memory += 1
+            i += 2
+        else:
+            total_memory += 1
+            i += 1
+
+p.answer_a = total_literals - total_memory
