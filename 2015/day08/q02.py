2c049763fa9dd18a503190a8032273a96bddbbf5
dexterity74 <justin.704504@gmail.com>
2025-04-08 00:08:07 -0500

    finished 2015 day08 part2

--- 2015/day08/q02.py
+++ 2015/day08/q02.py
@@ -8,5 +8,21 @@ from aocd.models import Puzzle
 p = Puzzle(day=8, year=2015)
 input = p.input_data.splitlines()
 
+total_literals = 0
+total_enc_literals = 0
 
-# p.answer_b = 
+def encode(line):
+    enc_len = 0
+    for i in range(len(line)):
+        if line[i] == "\"" or line[i] == "\\":
+            enc_len += 2
+        else:
+            enc_len += 1
+    return enc_len+2
+    
+
+for line in input:
+    total_literals += len(line)
+    total_enc_literals += encode(line)
+
+p.answer_b = total_enc_literals - total_literals
