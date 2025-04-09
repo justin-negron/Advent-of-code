3544d685514baaa4c8f6c3082f8930ddfc19c66f
dexterity74 <justin.704504@gmail.com>
2025-04-08 23:49:15 -0500

    init 2015 day12

---
+++ 2015/day12/q02.py
@@ -0,0 +1,12 @@
+"""
+--- Day 12: JSAbacusFramework.io ---
+https://adventofcode.com/2015/day/12
+"""
+from aocd.models import Puzzle
+
+
+p = Puzzle(day=12, year=2015)
+input = p.input_data.splitlines()
+
+
+# p.answer_b = 
