64c665d0eb9e415fdd57ff7034ae72cb75a79239
dexterity74 <justin.704504@gmail.com>
2025-04-08 23:48:36 -0500

    init 2015 day11

---
+++ 2015/day11/q01.py
@@ -0,0 +1,12 @@
+"""
+--- Day 11: Corporate Policy ---
+https://adventofcode.com/2015/day/11
+"""
+from aocd.models import Puzzle
+
+
+p = Puzzle(day=11, year=2015)
+input = p.input_data.splitlines()
+
+
+# p.answer_a = 
