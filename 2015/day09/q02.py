7e17c2f60743e4b10657705a6bbeafdffd1825f3
dexterity74 <justin.704504@gmail.com>
2025-04-09 22:47:22 -0500

    finished 2015 day09 part2

--- 2015/day09/q02.py
+++ 2015/day09/q02.py
@@ -3,10 +3,34 @@
 https://adventofcode.com/2015/day/9
 """
 from aocd.models import Puzzle
-
+from itertools import permutations
 
 p = Puzzle(day=9, year=2015)
 input = p.input_data.splitlines()
 
+def find_shortest_path(distances, cities):
+    longest = float('-inf')
+    for route in permutations(cities):
+        distance = sum(distances[route[i]][route[i+1]] for i in range(len(route)-1))
+        longest = max(longest, distance)
+    return longest
 
-# p.answer_b = 
+def parse_input(input):
+    distances = {}
+    cities = set()
+    for line in input:
+        city1, to, city2, eq, dist = line.split()
+        dist = int(dist)
+        if city1 not in distances:
+            distances[city1] = {}
+        if city2 not in distances:
+            distances[city2] = {}
+        distances[city1][city2] = dist
+        distances[city2][city1] = dist
+        cities.update([city1, city2])
+    return distances, cities
+
+distances, cities = parse_input(input)
+longest = find_shortest_path(distances, cities)
+
+p.answer_b = longest
