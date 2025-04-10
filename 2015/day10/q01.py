cbd18cb128ab5a423e9a12589b79b956f8e56f4c
dexterity74 <justin.704504@gmail.com>
2025-04-09 23:18:41 -0500

    finished 2015 day10 part1

--- 2015/day10/q01.py
+++ 2015/day10/q01.py
@@ -6,7 +6,27 @@ from aocd.models import Puzzle
 
 
 p = Puzzle(day=10, year=2015)
-input = p.input_data.splitlines()
+input = p.input_data
 
+def next_sequence(input):
+    if len(input) == 1:
+        return "".join(["1",input])
+    sequence = ""
+    prev = input[0]
+    dupe = 1
+    for num in input[1:]:
+        if num == prev:
+            dupe += 1
+        else:
+            sequence += "".join([str(dupe),str(prev)])
+            dupe = 1
+        prev = num
+    sequence += "".join([str(dupe),str(prev)])
+    return sequence
 
-# p.answer_a = 
+# perform sequence on input 40 times
+for i in range(40):
+    input = next_sequence(input)
+
+# answer = length of result
+p.answer_a = len(input)
