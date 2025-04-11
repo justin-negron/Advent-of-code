224f1bbeb2985bef40f5628efe4054a9111e4106
dexterity74 <justin.704504@gmail.com>
2025-04-10 22:58:28 -0500

    init 2015 day14

---
+++ 2015/day14/q01.py
@@ -0,0 +1,12 @@
+"""
+--- Day 14: Reindeer Olympics ---
+https://adventofcode.com/2015/day/14
+"""
+from aocd.models import Puzzle
+
+
+p = Puzzle(day=14,year=2015)
+input = p.input_data.splitlines()
+
+
+# p.answer_a = 
