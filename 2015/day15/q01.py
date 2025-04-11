d14af11abc6a3daff63b1bfc8f5b4f7d38e2cdc3
dexterity74 <justin.704504@gmail.com>
2025-04-10 22:59:46 -0500

    init 2015 day15

---
+++ 2015/day15/q01.py
@@ -0,0 +1,12 @@
+"""
+--- Day 15: Science for Hungry People ---
+https://adventofcode.com/2015/day/15
+"""
+from aocd.models import Puzzle
+
+
+p = Puzzle(day=15,year=2015)
+input = p.input_data.splitlines()
+
+
+# p.answer_a = 
