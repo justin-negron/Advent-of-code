46e16bdd5cb799b27ebd4976c050f2cb98ddf0bd
dexterity74 <justin.704504@gmail.com>
2025-04-10 22:57:51 -0500

    init 2015 day13

---
+++ 2015/day13/q01.py
@@ -0,0 +1,12 @@
+"""
+--- Day 13: Knights of the Dinner Table ---
+https://adventofcode.com/2015/day/13
+"""
+from aocd.models import Puzzle
+
+
+p = Puzzle(day=13,year=2015)
+input = p.input_data.splitlines()
+
+
+# p.answer_a = 
