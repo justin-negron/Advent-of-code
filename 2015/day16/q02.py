8ca166aea16fbee8e20c2fecd16c5f3788683b95
dexterity74 <justin.704504@gmail.com>
2025-04-11 23:24:31 -0500

    init 2015 day16

---
+++ 2015/day16/q02.py
@@ -0,0 +1,12 @@
+"""
+--- Day 16: Aunt Sue ---
+https://adventofcode.com/2015/day/17
+"""
+from aocd.models import Puzzle
+
+
+p = Puzzle(day=16,year=2015)
+input = p.input_data.splitlines()
+
+
+# p.answer_b = 
