9f391a76a42f66fb3f0868e48d57d361ab32d074
dexterity74 <justin.704504@gmail.com>
2025-04-12 23:47:00 -0500

    init 2015 day19

---
+++ 2015/day19/q01.py
@@ -0,0 +1,12 @@
+"""
+--- Day 19: Medicine for Rudolph ---
+https://adventofcode.com/2015/day/19
+"""
+from aocd.models import Puzzle
+
+
+p = Puzzle(day=19,year=2015)
+input = p.input_data.splitlines()
+
+
+# p.answer_a = 
---
+++ 2015/day19/q02.py
@@ -0,0 +1,13 @@
+"""
+--- Day 19: Medicine for Rudolph ---
+https://adventofcode.com/2015/day/19
+"""
+from aocd.models import Puzzle
+
+
+p = Puzzle(day=19,year=2015)
+input = p.input_data.splitlines()
+
+
+
+# p.answer_b = 
