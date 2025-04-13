bbce9858772af8e3a41e45f76411fb5dd5037955
dexterity74 <justin.704504@gmail.com>
2025-04-12 23:47:54 -0500

    init 2015 day20

---
+++ 2015/day20/q02.py
@@ -0,0 +1,13 @@
+"""
+--- Day 20: Infinite Elves and Infinite Houses ---
+https://adventofcode.com/2015/day/20
+"""
+from aocd.models import Puzzle
+
+
+p = Puzzle(day=20,year=2015)
+input = p.input_data.splitlines()
+
+
+
+# p.answer_b = 
