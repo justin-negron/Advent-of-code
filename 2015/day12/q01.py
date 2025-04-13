c0d3407a01d8d246919ef6187164cbfee63099dc
dexterity74 <justin.704504@gmail.com>
2025-04-13 02:02:24 -0500

    finished 2015 day12 part1

--- 2015/day12/q01.py
+++ 2015/day12/q01.py
@@ -3,10 +3,25 @@
 https://adventofcode.com/2015/day/12
 """
 from aocd.models import Puzzle
+import json
 
 
 p = Puzzle(day=12, year=2015)
-input = p.input_data.splitlines()
+input = p.input_data
 
+total = 0
+is_neg = False
+num = ""
+for char in input:
+    if char == '-':
+        is_neg = True
+    elif char.isdigit():
+        num += char
+    else:
+        if num != "":
+            total += -int(num) if is_neg else int(num)
+        
+        is_neg = False
+        num = ""
 
-# p.answer_a = 
+p.answer_a = total
