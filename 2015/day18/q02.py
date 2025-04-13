6a3d2ff52b45756280cf81730fd07a6d468d98a7
dexterity74 <justin.704504@gmail.com>
2025-04-12 23:46:16 -0500

    init 2015 day18

---
+++ 2015/day18/q02.py
@@ -0,0 +1,12 @@
+"""
+--- Day 18: Like a GIF For Your Yard ---
+https://adventofcode.com/2015/day/18
+"""
+from aocd.models import Puzzle
+
+
+p = Puzzle(day=18,year=2015)
+input = p.input_data.splitlines()
+
+
+# p.answer_b = 
