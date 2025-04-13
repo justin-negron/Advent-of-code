8101ac5153429964bb7db747f8558b19fe105c04
dexterity74 <justin.704504@gmail.com>
2025-04-13 02:15:27 -0500

    work in progress 2015 day12 part2

--- 2015/day12/q02.py
+++ 2015/day12/q02.py
@@ -3,10 +3,15 @@
 https://adventofcode.com/2015/day/12
 """
 from aocd.models import Puzzle
+import json
 
 
 p = Puzzle(day=12, year=2015)
-input = p.input_data.splitlines()
+input = p.input_data
+
+# traverse input like stack
+    # if "red" between {}, ignore until } found
+
 
 
 # p.answer_b = 
