96cf9b22864bbf96295db794ba93093d93118705
dexterity74 <justin.704504@gmail.com>
2025-04-14 00:20:28 -0500

    finished 2015 day12 part2

--- 2015/day12/q02.py
+++ 2015/day12/q02.py
@@ -9,9 +9,15 @@ import json
 p = Puzzle(day=12, year=2015)
 input = p.input_data
 
-# traverse input like stack
-    # if "red" between {}, ignore until } found
+def recursum(input):
+    match input:
+        case dict():
+            values = list(input.values())
+            return 0 if "red" in values else recursum(values)
+        case list():
+            return sum(recursum(num) for num in input)
+        case int():
+            return input
+        case _: return 0
 
-
-
-# p.answer_b = 
+p.answer_b = recursum(json.loads(input))
