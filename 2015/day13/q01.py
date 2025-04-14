089980aaacc7363f2a070115869940c3369a017d
dexterity74 <justin.704504@gmail.com>
2025-04-14 00:37:56 -0500

    work in progress 2015 day 13 part1

--- 2015/day13/q01.py
+++ 2015/day13/q01.py
@@ -8,5 +8,8 @@ from aocd.models import Puzzle
 p = Puzzle(day=13,year=2015)
 input = p.input_data.splitlines()
 
+for line in input:
+    p1, _, mood, points, _, _, _, _, _, _, p2 = line[:-1].split()
+    print(p1, mood, points, p2)
 
 # p.answer_a = 
