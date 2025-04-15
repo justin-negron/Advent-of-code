4e5e31bfdfc91a2edd62417b5c05f6dcb044bf87
dexterity74 <justin.704504@gmail.com>
2025-04-14 23:46:22 -0500

    init 2015 day21

---
+++ 2015/day21/q02.py
@@ -0,0 +1,12 @@
+"""
+--- Day 21: RPG Simulator 20XX ---
+https://adventofcode.com/2015/day/21
+"""
+from aocd.models import Puzzle
+
+
+p = Puzzle(day=21,year=2015)
+input = p.input_data.splitlines()
+
+
+# p.answer_b = 
