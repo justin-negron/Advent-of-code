2c8c559683fd84a60bd5b05a4bf8bbebcaa3d8c6
dexterity74 <justin.704504@gmail.com>
2025-04-14 23:47:03 -0500

    init 2015 day22

---
+++ 2015/day22/q02.py
@@ -0,0 +1,12 @@
+"""
+--- Day 22: Wizard Simulator 20XX ---
+https://adventofcode.com/2015/day/22
+"""
+from aocd.models import Puzzle
+
+
+p = Puzzle(day=22,year=2015)
+input = p.input_data.splitlines()
+
+
+# p.answer_b = 
